3ecbcab19145977899afa3298db1fdc46c5d0cfc
presidenTelkom <akunpoteam@gmail.com>
2024-06-11 06:03:46 +0700

    test git ke 3

--- app/src/main/java/org/d3if3068/buildyourfirstapp/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3068/buildyourfirstapp/ui/screen/MainScreen.kt
@@ -131,6 +131,9 @@ fun ScreenContent(modifier: Modifier) {
                 contentAlignment = Alignment.Center
             ) {
                 CircularProgressIndicator()
+                Column {
+
+                }
             }
         }
 
